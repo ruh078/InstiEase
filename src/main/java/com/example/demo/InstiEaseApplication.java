ebe78b14e230ebb36238c32ac4733beea8525e72
ruh078 <ruhijoshi807@gmail.com>
2022-09-27 01:50:54 +0530

    dbms

---
+++ src/main/java/com/example/demo/InstiEaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstiEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstiEaseApplication.class, args);
+	}
+
+}
