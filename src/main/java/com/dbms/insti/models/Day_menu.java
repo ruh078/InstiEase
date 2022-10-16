2047804bb8f48686b237cd509f92e92ed004bce1
ruh078 <ruhijoshi807@gmail.com>
2022-10-16 17:28:37 +0530

    changes to model

--- src/main/java/com/dbms/insti/models/Day_menu.java
+++ src/main/java/com/dbms/insti/models/Day_menu.java
@@ -1,5 +1,39 @@
 package com.dbms.insti.models;
 
 public class Day_menu {
-
+	private int mess_id;
+	private int day_id;
+	private String breakfast;
+	private String lunch;
+	private String dinner;
+	public int getMess_id() {
+		return mess_id;
+	}
+	public void setMess_id(int mess_id) {
+		this.mess_id = mess_id;
+	}
+	public int getDay_id() {
+		return day_id;
+	}
+	public void setDay_id(int day_id) {
+		this.day_id = day_id;
+	}
+	public String getBreakfast() {
+		return breakfast;
+	}
+	public void setBreakfast(String breakfast) {
+		this.breakfast = breakfast;
+	}
+	public String getLunch() {
+		return lunch;
+	}
+	public void setLunch(String lunch) {
+		this.lunch = lunch;
+	}
+	public String getDinner() {
+		return dinner;
+	}
+	public void setDinner(String dinner) {
+		this.dinner = dinner;
+	}
 }
--- src/main/java/com/dbms/insti/models/Medicine.java
+++ src/main/java/com/dbms/insti/models/Medicine.java
@@ -1,5 +1,25 @@
 package com.dbms.insti.models;
 
 public class Medicine {
-
+	private int medicine_id;
+	private String medicine_name;
+	public int getMedicine_id() {
+		return medicine_id;
+	}
+	public void setMedicine_id(int medicine_id) {
+		this.medicine_id = medicine_id;
+	}
+	public String getMedicine_name() {
+		return medicine_name;
+	}
+	public void setMedicine_name(String medicine_name) {
+		this.medicine_name = medicine_name;
+	}
+	public String getQuantity_in_stock() {
+		return quantity_in_stock;
+	}
+	public void setQuantity_in_stock(String quantity_in_stock) {
+		this.quantity_in_stock = quantity_in_stock;
+	}
+	private String quantity_in_stock;
 }
