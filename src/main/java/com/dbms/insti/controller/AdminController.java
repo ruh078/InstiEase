bc5f182af8473fd001debe4d83e504408ff1d616
OREO1210 <bittoo979@gmail.com>
2022-10-17 01:10:17 +0530

    test

---
+++ src/main/java/com/dbms/insti/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.dbms.insti.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dbms.insti.models.Users;
+import com.dbms.insti.service.SecurityService;
+import com.dbms.insti.service.UserService;
+
+public class AdminController {
+    @Autowired
+    private SecurityService securityService;
+    @Autowired
+    private UserService userService;
+   @GetMapping("/admin")
+   public String adminpage(Model model){
+       if(securityService.isLoggedIn()) {
+           if(userService.findByEmail(securityService.findLoggedInUsername()).getRole()==1) {
+               return "admin_profile";
+           }
+           return "redirect:/";
+       }
+       
+       return "redirect:/login";
+       
+   }
+}
