3b71b72e368e32c40d3621a8f7bba486dbc783a5
OREO1210 <bittoo979@gmail.com>
2022-10-17 18:03:39 +0530

    ui added

--- src/main/java/com/dbms/insti/controller/AdminStudentController.java
+++ src/main/java/com/dbms/insti/controller/AdminStudentController.java
@@ -1,25 +1,24 @@
 package com.dbms.insti.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.dbms.insti.models.Hostel;
-import com.dbms.insti.service.HostelService;
 import com.dbms.insti.service.SecurityService;
 import com.dbms.insti.service.UserService;
-
+@Controller
 public class AdminStudentController {
     @Autowired
     private SecurityService securityService;
     @Autowired
     private UserService userService;
     
-    @GetMapping("/admin/hostel")
-    public String adminhostel(Model model) {
+    @GetMapping("/admin/student")
+    public String adminstudent(Model model) {
         if(securityService.isLoggedIn()) {
             if(userService.findByEmail(securityService.findLoggedInUsername()).getRole()==1) {
-               
+                
                 return "admin_student";
             }
             return "redirect:/";
